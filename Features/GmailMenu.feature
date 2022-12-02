3043eff8e8f38afb99dc29222aed1def7b6ed876
Mikita Hauruk <Mikita_Hauruk@epam.com>
2022-12-02 21:22:34 +0300

    Added BDD steps

---
+++ Features/GmailMenu.feature
@@ -0,0 +1,25 @@
+﻿Feature: GmailMenu
+	In order to mail usage
+	As a specflow user
+	I want to use gmail 
+
+@smoke
+Scenario: Navigate to page and verify title
+	Given Login to mail
+	When Navigate to 'Drafts'
+	Then Verify that 'Drafts' title is correct
+	And Log out
+
+@smoke
+Scenario Outline: Send message from gmail main menu and verify it
+	Given Login to mail
+	When Write mail to '<To>' with '<Subject>' subject and '<BodyMail>' bodyMail
+	When Send mail
+	Then Navigate to Sent page
+	And Verfiy mail with '<Subject>' subject succesfully sent
+	And Log out
+
+	Examples: 
+	| To                   | Subject | BodyMail |
+	| gavruk1337@gmail.com | Hi!     | How are you! |
+	| gavruk1337@gmail.com | Did not get answer     | Why you did not reply me? :( |
---
+++ StepDefinitions/GmailMenuStepDefinitions.cs
@@ -0,0 +1,80 @@
+using GmailTA.Pages;
+using GmailTA.WebDrvier;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowGmailProject.Specs.StepDefinitions
+{
+    [Binding]
+    public class GmailMenuStepDefinitions
+    {
+        private ChooseAnAccountPage _chooseAnAccountPage = new ChooseAnAccountPage();
+        private LoginPage _loginPage = new LoginPage();
+        private SentPage _sentPage = new SentPage();
+        private ComposeMessagePage _composeMessagePage = new ComposeMessagePage();
+        private MainPage _mainPage = new MainPage();
+        protected static Browser Browser = Browser.Instance;
+
+        [BeforeTestRun]
+        public static void CreateWebDriver()
+        {
+            Browser = Browser.Instance;
+            Browser.WindowMaximaze();
+            Browser.NavigateTo(Configuration.StartUrl);
+        }
+
+        [Given(@"Login to mail")]
+        public void LoginToMail()
+        {
+            _loginPage.Login();
+        }
+
+        [When(@"Navigate to '([^']*)'")]
+        public void NavigateToPage(string page)
+        {
+            _mainPage.NavigateToPage(page);
+        }
+
+        [Then(@"Verify that '([^']*)' title is correct")]
+        public void VerifyThatTitleIsCorrect(string title)
+        {
+            Assert.True(AbstractPage.IsPageLoadedByTitle(title));
+        }
+
+
+        [When(@"Write mail to '(.*)' with '(.*)' subject and '(.*)' bodyMail")]
+        public void WriteMailToWithSubjectAndBodyMail(string to,string subject, string bodyMail)
+        {
+            _composeMessagePage.FillFullMail(to, subject, bodyMail);
+        }
+
+        [When(@"Send mail")]
+        public void SendMail()
+        {
+            _composeMessagePage.SendMail();
+        }
+        [Then(@"Navigate to Sent page")]
+        public void ThenNavigateToSentPage()
+        {
+            _mainPage.NavigateToSentPage();
+        }
+
+        [Then(@"Verfiy mail with '(.*)' subject succesfully sent")]
+        public void VerfiyMailWithSubjectSuccesfullySent(string subject)
+        {
+            Assert.IsTrue(_sentPage.VerfiyMailAsSent(subject));
+        }
+
+        [Then(@"Log out")]
+        public void LogOut()
+        {
+            _chooseAnAccountPage.LogoutFromAccount();
+        }
+        [AfterTestRun]
+        public static void TestTearDown()
+        {
+            Browser.QuiteBrowser();
+        }
+    }
+}
